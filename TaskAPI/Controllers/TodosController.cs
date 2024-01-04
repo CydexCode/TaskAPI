bc1924844bb0d72acbbb60a41ceb3502d05026b2
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-01-04 21:52:29 +0530

    get all data

---
+++ TaskAPI/Controllers/TodosController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using TaskAPI.Models;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            var mytodos = AllTodos();
+            return Ok(mytodos);
+        }
+
+        //Get Todos
+        private List<Todo> AllTodos()
+        {
+            var todos = new List<Todo>();
+
+            var todo1 = new Todo
+            {
+                Id = 1,
+                Title = "Test Title 01",
+                Description = "Test Description 01",
+                CreatedDate = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+
+            };
+            todos.Add(todo1);
+
+            var todo2 = new Todo
+            {
+                Id = 2,
+                Title = "Test Title 01",
+                Description = "Test Description 01",
+                CreatedDate = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+
+            };
+
+            todos.Add(todo2);
+
+            var todo3 = new Todo
+            {
+                Id = 3,
+                Title = "Test Title 01",
+                Description = "Test Description 01",
+                CreatedDate = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+
+            };
+            todos.Add(todo3);
+
+            return todos;
+
+        }
+    }
+}
