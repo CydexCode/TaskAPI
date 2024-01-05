e1c4ab32af6636ab281351dcd6cacb8fd4cedccf
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-01-05 13:32:52 +0530

    Create DataAccess  Layer , code first

---
+++ TaskAPI.DataAccess/Migrations/20240105074938_Seedata2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskAPI.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seedata2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "CreatedDate", "Description", "Due", "Status", "Title" },
+                values: new object[] { 1, new DateTime(2024, 1, 5, 13, 19, 38, 41, DateTimeKind.Local).AddTicks(1128), "Test Description 01", new DateTime(2024, 1, 10, 13, 19, 38, 41, DateTimeKind.Local).AddTicks(1148), 0, "Test Title 01" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
